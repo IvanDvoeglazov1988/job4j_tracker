3f314552fb26c8c2824ad96da49c2901a68c9f3a
IvanDvoeglazov1988 <id@stk-online.ru>
2022-12-23 16:00:59 +0500

    1.3. Вызов метода с аргументами. [#166086 #373923]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Состояние: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 1, "Error");
+        error.printInfo();
+        Error errorOne = new Error();
+        errorOne.printInfo();
+    }
+}
