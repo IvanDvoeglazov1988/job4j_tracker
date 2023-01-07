c715cf0b01c45742f46825cf6546c6f1631039b5
IvanDvoeglazov1988 <id@stk-online.ru>
2023-01-07 11:35:13 +0500

    4.2. Интерфейс Input [#181092 #199985]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,28 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void go() {
+        System.out.println("Автобус поехал");
+
+    }
+
+    @Override
+    public void man(int num) {
+        System.out.println("Кол-во пасажиров: " + num);
+    }
+
+    @Override
+    public int ref(int benz) {
+        int price = 55;
+        return benz * price;
+    }
+
+    public static void main(String[] args) {
+        Transport bus = new Bus();
+        bus.go();
+        bus.man(10);
+        System.out.println("Затраты на бензин: " + bus.ref(100));
+    }
+}
